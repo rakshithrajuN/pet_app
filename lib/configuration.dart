265d8101a9894d49dcf3ae764a64e63a0aaf6649
ssumanthh <sumanthprabhu2001@gmail.com>
2021-07-29 22:23:00 +0530

    sloved error

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-Color primaryGreen =  Color(0xff416d6d);
+Color primaryGreen = Color(0xff416d6d);
 List<BoxShadow> shadowList = [
-  BoxShadow(color: Colors.grey,blurRadius: 30,offset: Offset(0, 10))
+  BoxShadow(color: Colors.grey, blurRadius: 30, offset: Offset(0, 10))
 ];
 
-List<Map> categories =[
-  {'name':'Dogs','iconPath': 'android/images/dog.png'},
-  {'name':'Cats','iconPath': 'android/images/cat.png'},
-  {'name':'Bird','iconPath': 'android/images/scarlet-macaw.png'},
-  {'name':'Rabbit','iconPath': 'android/images/rabbit.png'},
-
+List<Map> categories = [
+  {'name': 'Dogs', 'iconPath': 'assets/dog.png'},
+  {'name': 'Cats', 'iconPath': 'assets/cat.png'},
+  {'name': 'Bird', 'iconPath': 'assets/scarlet-macaw.png'},
+  {'name': 'Rabbit', 'iconPath': 'assets/rabbit.png'},
 ];
