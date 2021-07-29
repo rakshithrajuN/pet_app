a418faaabfc3deaf2de9a1196f881937a7a2cc2b
rakshithrajuN <rakshithraju2000@gmail.com>
2021-07-29 16:54:10 +0530

    list added

---
+++ lib/configuration.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color primaryGreen =  Color(0xff416d6d);
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey,blurRadius: 30,offset: Offset(0, 10))
+];
+
+List<Map> categories =[
+  {'name':'Dogs','iconPath': 'android/images/dog.png'},
+  {'name':'Cats','iconPath': 'android/images/cat.png'},
+  {'name':'Bird','iconPath': 'android/images/scarlet-macaw.png'},
+  {'name':'Rabbit','iconPath': 'android/images/rabbit.png'},
+
+];
