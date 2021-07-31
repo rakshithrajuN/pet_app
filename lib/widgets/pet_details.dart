0fff8a1d76c85eee962eeaa1c0ba9235db609810
rakshithrajuN <rakshithraju2000@gmail.com>
2021-07-31 11:19:43 +0530

    pet details finished

--- lib/widgets/pet_details.dart
+++ lib/widgets/pet_details.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Petdetails extends StatefulWidget {
-   Petdetails({ Key? key,required this.petname,required this.breed,required this.year }) : super(key: key);
+  Petdetails(
+      {Key? key,
+      required this.petname,
+      required this.breed,
+      required this.year})
+      : super(key: key);
   final String petname;
   final String breed;
   final String year;
@@ -15,22 +20,171 @@ class _PetdetailsState extends State<Petdetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.blueGrey[300],
-                  
+          child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blueGrey[300],
+            ),
+          ),
+          Expanded(
+              child: Container(
+            color: Colors.white,
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Petname:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.petname,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Expanded(
-                child: Container(
-                  color: Colors.white,
-                  child: Image.asset('assets/buldog.jpg'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Breed:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.breed,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Petname:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.year,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Price:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.petname,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Location:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.petname,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Phone Number:',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      widget.petname,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text('Adopt Pet'),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0)),
+                    color: Colors.blueGrey[300],
+                  ),
                 ),
               )
-            ],
-          )),
-      );
+            ]),
+          ))
+        ],
+      )),
+    );
   }
 }
