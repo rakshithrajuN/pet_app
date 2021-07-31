224df51b2b8466103ef224850b715939989334fd
rakshithrajuN <rakshithraju2000@gmail.com>
2021-07-31 20:31:22 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,29 @@
-import 'package:flutter/material.dart';
-import 'authentication/auth.dart';
-import 'authentication/root_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/services.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 import 'DrawerScreen.dart';
 import 'HomeScreen.dart';
 
 void main() async{
-=======
-void main() async {
->>>>>>> a3782376d0e6e774699dd53e29978a0fb47e22da
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  WidgetsFlutterBinding.ensureInitialized();
-   await FirbaseAppCheck.instance.activate(webRecaptchaSiteKey: 'recaptcha-v3-site-key');
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(new MyApp());
-  });
+  Firebase.initializeApp();
+  runApp(new MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FoodCourt',
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: RootPage(
-        auth: new Auth(),
-        st: 'Register',
-      ),
+       home:    Scaffold(
+        body: Stack(
+          children: [
+            DrawerScreen(),
+            HomeScreen()
+        ],
+      )
+       )     
     );
+      
   }
 }
